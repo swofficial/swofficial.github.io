c93dddd2b3679e5d6bdbceebcdbcdc46908fb708
swofficial <izonit546@gmail.com>
2024-07-09 19:40:11 +0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    new fullpage('#fullpage', {
+        autoScrolling: true,
+        navigation: true,
+        navigationPosition: 'left',
+        navigationTooltips: ['Stars World', 'Info', 'Join us', 'End'],
+        showActiveTooltip: false,
+        scrollingSpeed: 700
+    });
+});
